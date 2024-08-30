ddae4dca33233e90375abe80aeee455afc5ee170
pw52lvf <pw52lvf@eduvaud.ch>
2024-08-30 10:17:03 +0200

    commit

---
+++ personnel/ex_escargot/ex_escargot/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ex_escargot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(0, 15);
+            Console.Write("_@_ö");
+            int vie = 50;
+            for (int i = 0; i < vie; i++)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(i, 15);
+                Console.Write("_@_ö");
+                Thread.Sleep(100);
+            }
+            Console.Clear();
+            Console.SetCursorPosition(vie, 15);
+            Console.Write("____");
+            Console.Read();
+        }
+    }
+}
