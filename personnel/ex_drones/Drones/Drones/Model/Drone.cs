cfe38ff4eefbc67e35bc4666d05cf549fa319182
pw52lvf <pw52lvf@eduvaud.ch>
2024-09-27 12:14:01 +0200

    fin de séquence 5

--- personnel/ex_drones/Drones/Drones/Model/Drone.cs
+++ personnel/ex_drones/Drones/Drones/Model/Drone.cs
@@ -7,17 +7,17 @@ namespace Drones
     public partial class Drone : IExpellable
     {
         Random alea = new Random();
-
         private int charge = 1000;                     // La charge actuelle de la batterie
         private string name;                           // Un nom
         private int x ;                                // Position en X depuis la gauche de l'espace aérien
         private int y;                                 // Position en Y depuis le haut de l'espace aérien
+        private EvacuationState state;
 
         public Drone(int x, int y)
         {
             this.x = x;
             this.y = y;
-            this.name = name;
+            state = EvacuationState.Free;
         }
         public void Attributs(int x, int y, string name)
         {
@@ -28,17 +28,26 @@ namespace Drones
 
         public bool Evacuate(Rectangle zone)
         {
-            throw new NotImplementedException();
+            if (zone.IntersectsWith(new Rectangle(x - 4, y - 2, 8, 8)))
+            {
+                state = EvacuationState.Evacuating;
+                return false;
+            }
+            else
+            {
+                state = EvacuationState.Evacuated;
+                return true;
+            }
         }
 
         public void FreeFlight()
         {
-            throw new NotImplementedException();
+            state = EvacuationState.Free;
         }
 
         public EvacuationState GetEvacuationState()
         {
-            throw new NotImplementedException();
+            return state;
         }
 
         // Cette méthode calcule le nouvel état dans lequel le drone se trouve après
